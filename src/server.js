d0e8ba9011e02c6a6a2764770b89759e563a0767
Ana Cremasco <ana.c.luiz@aluno.senai.br>
2026-06-15 11:09:24 -0300

    feat: Continua atividade

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'dotenv/config';
+import exemplosRoutes from './routes/exemploRoute.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('🚀 API funcionando');
+});
+
+
+// Rotas
+import { apiKey } from './lib/middlewares/apiKey.js';
+
+app.use('/api/exemplos', apiKey, exemplosRoutes);
+
+import arquivoRoutes from './routes/arquivoRoute.js';
+
+app.use('/api/exemplos', apiKey, arquivoRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Rota não encontrada' });
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
